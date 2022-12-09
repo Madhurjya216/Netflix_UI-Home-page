c98ac2f02e2a111e8b722fb5b8bb0c9158ef3959
Madhurjya216 <madhurjyakashyap216@gmail.com>
2022-12-09 19:02:33 +0530

    code

---
+++ script.js
@@ -0,0 +1,10 @@
+// let para = document.getElementsByClassName("para");
+// let flex = document.getElementsByClassName("flex")
+function toggle(){
+let para = document.getElementsByClassName("para");
+    if (para.style.display == "none"){
+        para.style.display = "block";
+    }else{
+        para.style.display = "none";
+    }
+} 
